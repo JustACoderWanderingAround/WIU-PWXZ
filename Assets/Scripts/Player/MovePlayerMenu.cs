73fa168298ba1ff2456f0dcc31d5e41dcee5a59a
222839K <222839K@mymail.nyp.edu.sg>
2024-02-14 19:38:35 +0800

    updated menu scene

---
+++ Assets/Scripts/Player/MovePlayerMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayerMenu : MonoBehaviour
+{
+    private float moveSpeed = 200f;
+    [SerializeField] private Transform waypoint;
+    //[SerializeField] private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float moveDistance = moveSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * moveDistance);
+        Debug.Log(moveDistance);
+        if (transform.position.x > 1500.0f)
+        {
+            transform.position = waypoint.position;
+
+        }
+    }
+}
