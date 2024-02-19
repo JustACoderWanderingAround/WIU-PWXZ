d71773b2712f5fd13e4bf7f3c35704ffd3233e0e
JustACoderWanderingAround <pgbaquiran@gmail.com>
2024-02-19 14:17:54 +0800

    converted doors to interactables - started on ground level design

--- Assets/Scripts/LevelProp/MiniGamePicker.cs
+++ Assets/Scripts/LevelProp/MiniGamePicker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// <summary>
 /// This class randomises and picks a minigame based on the attached list of minigames.
 /// </summary>
-public class MiniGamePicker : MonoBehaviour
+public class MiniGamePicker : MonoBehaviour, IInteractable
 {
     [SerializeField] GameObject miniGameContainer;
     [SerializeField] GameObject alertBoxCanvas;
@@ -22,36 +22,43 @@ public class MiniGamePicker : MonoBehaviour
         chosenGame = Random.Range(0, miniGameContainer.transform.childCount);
         miniGameContainer.transform.GetChild(chosenGame).gameObject.SetActive(true);
     }
-    void Update()
-    {
-        if (withinRange)
-        {
-            if (!alertBoxCanvas.activeInHierarchy && !chosen)
-                alertBoxCanvas.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                Activate();
-                chosen = true;
-            }
-        }
-        else
-        {
-            if (alertBoxCanvas.activeInHierarchy || chosen)
-                alertBoxCanvas.SetActive(false);
-        }
+    void Update() { 
+    //{
+    //    if (withinRange)
+    //    {
+    //        if (!alertBoxCanvas.activeInHierarchy && !chosen)
+    //            alertBoxCanvas.SetActive(true);
+    //        if (Input.GetKeyDown(KeyCode.E))
+    //        {
+               
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (alertBoxCanvas.activeInHierarchy || chosen)
+    //            alertBoxCanvas.SetActive(false);
+    //    }
     }
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-            withinRange = true;
+        if (collision.gameObject.CompareTag("Player"))  
+            if (!alertBoxCanvas.activeInHierarchy && !chosen)
+                alertBoxCanvas.SetActive(true);
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
-            withinRange = false;
+            if (alertBoxCanvas.activeInHierarchy || chosen)
+                alertBoxCanvas.SetActive(false);
     }
     public void Reset()
     {
         chosen = false;
+    }
+
+    public void OnInteract()
+    {
+        Activate();
+        chosen = true;
     }
 }
