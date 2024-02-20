a02d91f5236c86339d0095c77986d9245b56c98c
JustACoderWanderingAround <pgbaquiran@gmail.com>
2024-02-20 13:48:34 +0800

    lots and lots of things

---
+++ Assets/Scripts/Interactables/Door.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Collider doorCollider;
+    private bool isOpen = false;
+
+    private void Awake()
+    {
+        //animator = GetComponent<Animator>();
+    }
+    public void OnInteract()
+    {
+        isOpen = !isOpen;
+
+        if (isOpen)
+        {
+            doorCollider.isTrigger = true;
+            animator.SetTrigger("open");
+        }
+        else if (!isOpen)
+        {
+            doorCollider.isTrigger = false;
+            animator.SetTrigger("close");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            doorCollider.isTrigger = true;
+            animator.SetTrigger("open");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            doorCollider.isTrigger = false;
+            animator.SetTrigger("close");
+        }
+    }
+}
+
