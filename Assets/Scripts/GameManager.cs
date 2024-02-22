3fd66ba53df0fbbb848c2cb44af4b4567b6b645e
JustACoderWanderingAround <pgbaquiran@gmail.com>
2024-02-22 11:02:31 +0800

    added gamecontroller and added camera actions

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager
+{
+    private static List<GameObject> EvidenceList;
+
+    public static void Init()
+    {
+        if (EvidenceList == null)
+        {
+            EvidenceList = new List<GameObject>();
+        }
+    }
+
+    public static void AddEvidence(GameObject[] newEvidenceList)
+    {
+        bool withinList = false;
+        foreach (GameObject newEvidence in newEvidenceList)
+        {
+            foreach (GameObject evidence in EvidenceList)
+            {
+                if (evidence == newEvidence)
+                {
+                    withinList = true;
+                    break;
+                }
+            }
+            if (!withinList)
+            {
+                EvidenceList.Add(newEvidence);
+            }
+        }
+        
+    }
+}
